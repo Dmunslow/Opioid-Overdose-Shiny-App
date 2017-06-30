abff5c1ce5ae451090aa70dece37fa7df752a009
Dmunslow <dmunslow11@gmail.com>
2017-06-29 23:43:28 -0600

    Change Color Scheme/rename variables

--- Opioid_Overdoses/server.R
+++ Opioid_Overdoses/server.R
@@ -13,7 +13,7 @@ require(plotly)
 
 #Load overdose data 
 overdose_states <- read.csv("./data/overdose_processed_states.csv", header = T)
-overdose_US <- read.csv("./data/overdose_processed_US.csv",header = T)
+overdose_states <- overdose_states[,1:6]
 
 
 library(shiny)
@@ -25,30 +25,33 @@ shinyServer(function(input, output) {
     displayYear <- reactive({
         input$Year
     })
-    stat_type <- reactive(({
-        input$statType
-    }))
+    stat_type <- reactive({
+            input$statType
+    })
+    
     
     # Reactive label for the choropleth
     output$mapYear <- renderText({
         stat <- stat_type()
-        if(stat == "PCT_of_Total_Deaths"){
-            paste("Opioid Overdoses as % of all Deaths in ", displayYear())
-        } else if (stat == "Crude_Rate"){
-            paste("Opioid Overdose Death Rate in ", displayYear())
+        
+        if(stat == "Pct_of_Total_Deaths"){
+            paste("Opioid Overdoses as % of all Deaths in", displayYear())
+        } else if (stat == "Death_Rate"){
+            paste("Opioid Overdose Death Rate in", displayYear())
         } else if (stat == "Deaths") {
-            paste("Total Opioid Overdose Deaths in ", displayYear())
+            paste("Total Opioid Overdose Deaths in", displayYear())
         }
     })
     
     # Reactive label for the data table
     output$tableYear <- renderText({
-        paste("Opioid Overdose Statistics for the Year ", displayYear())
+        paste("Opioid Overdose Statistics for the Year", displayYear())
     })
     
     # reactive function that subsets the data based on slider and radio buttons
     df_subset <- reactive({
         data <- overdose_states[(overdose_states$Year == displayYear() & overdose_states$Multiple_Cause_of_death == input$odType),]
+        
         return(data)
     })
     
@@ -58,10 +61,14 @@ shinyServer(function(input, output) {
     output$choropleth <- renderGvis({
         data <- df_subset()
         stat <- stat_type()
+    
         gvisGeoChart(data, "State", stat, options = list(region="US",
                                                              displayMode = "regions",
                                                              resolution = "provinces",
-                                                            width = 500, height = 300))
+                                                             colors = "['#FFFFCC', '#FFEDA0', '#FED976', '#FEB24C',
+                                                                  '#FD8D3C', '#FC4E2A', '#E31A1C', '#BD0026', '#800026']",
+                                                             width = 500, 
+                                                             height = 300))
     })
     
     ### Create table that outputs data based on selected radio buttons
--- Opioid_Overdoses/ui.R
+++ Opioid_Overdoses/ui.R
@@ -27,18 +27,28 @@ shinyUI(fluidPage(
                      "Methadone" = "Methadone" )),
       radioButtons("statType", "Choose Statistic:",
                          c("Total Deaths" = "Deaths", 
-                           "Death Rate (per 100,000)" = "Crude_Rate",
-                           "As Percentage of All Deaths" = "PCT_of_Total_Deaths")),
+                           "Death Rate (per 100,000)" = "Death_Rate",
+                           "As Percentage of All Deaths" = "Pct_of_Total_Deaths")),
       h3(tableOutput("tableYear")),
       htmlOutput('myTable')
       ),
       
   mainPanel(
-      sliderInput("Year", "Select Year to be displayed:",
+     h2("How to Use this App"),
+     p("This app uses data from the Centers For Disease Control to demonstrate how the opioid crisis in the US has evolved over time.
+        Users may select the type of opioid overdose, as well as the statistic to be displayed in the side panel.
+        The year can be changed using the slider, or a time sequence can viewed by pressing the play button.
+        Users may see the data being mapped for each state by hovering over the state or by referencing the table in the side panel.
+        More information on this epidemic can be found by following the link below the map."),
+     
+     sliderInput("Year", "Select Year to be displayed:",
                   min = 2000, max =2015, value = 2000, step = 1,
                   animate = TRUE, sep = "", width = 500 ),
      h3(textOutput("mapYear")),
-     htmlOutput("choropleth")
+     htmlOutput("choropleth"),
+     helpText( p("Note: States which appear green on the map represent unavailable data for that year/overdose type"),
+               a("Centers for Disease Control and Prevention: Opioid Overdoses", 
+                 href = "https://www.cdc.gov/drugoverdose/index.html", target ="_blank" ))
   )
   
  
